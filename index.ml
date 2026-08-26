a359808a137e7dceb4aca06bdbf514659054aeec
bobbyhenkelmann <60762211+bobbyhenkelmann@users.noreply.github.com>
2026-08-26 15:49:53 -0400

    Create index.ml

---
+++ index.ml
@@ -0,0 +1,9 @@
+---
+layout: home
+title: Home
+---
+
+I'm an ECE student intereseted in embedded systems, DSP, and power electronics. 
+
+## Projects
+- [Underwater Acoustic Modem](/projects/acoustic-modem)
